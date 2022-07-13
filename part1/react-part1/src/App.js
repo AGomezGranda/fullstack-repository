056e96a88ba9fd70175343afbda4167d7edfb030
AGomezGranda <a.gomez191@usp.ceu.es>
2022-07-13 13:02:05 +0200

    part 1.C - completed

--- part1/react-part1/src/App.js
+++ part1/react-part1/src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
-const Display = (counter) => <div> {counter} </div>
-const Button = (onClick, text) =>  <button onClick={onClick}> {text} </button>
+const Display = ({counter}) => <div> {counter} </div>
+const Button = ({onClick, text}) => <button onClick={onClick}> {text} </button>  
 
 const App = () => {
   const [counter, setCounter] = useState(0);
